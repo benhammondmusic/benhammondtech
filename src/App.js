748e0f9415def65d640494f0a10e5243cc317e85
BenHammondMusic <41567007+benhammondmusic@users.noreply.github.com>
2021-03-17 00:10:54 -0600

    styling

--- src/App.js
+++ src/App.js
@@ -3,16 +3,29 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <main className="flex items-center justify-center h-screen">
-        <div className="flex flex-column flex-wrap align-center">
-          <a href="https://docs.google.com/document/d/1nYJf1ZjGetUo8lFbwoyf23prkTw-0H_HiqvMKlY5_nw/edit?usp=sharing">resume - google doc</a>
-          <a href="https://blog.benhammond.tech">blog - music and tech</a>
+      <main className=" flex flex-col items-center justify-center h-screen">
+        <h1 className="my-20 font-sans">benhammond.tech - denver developer</h1>
+        <div className="w-2/3">
+          <ul className="transform rotate-180 flex flex-col items-center">
+            <li>
+              <a href="https://docs.google.com/document/d/1nYJf1ZjGetUo8lFbwoyf23prkTw-0H_HiqvMKlY5_nw/edit?usp=sharing">resume</a>
+            </li>
+            <li>
+              <a href="https://blog.benhammond.tech">blog</a>
+            </li>
 
-          <a href="https://github.com/benhammondmusic">github - projects and code playgrounds</a>
+            <li>
+              <a href="https://github.com/benhammondmusic">github</a>
+            </li>
 
-          <a href="https://www.benhammondmusic.com">benhammondmusic - professional musician site</a>
+            <li>
+              <a href="https://www.benhammondmusic.com">music</a>
+            </li>
 
-          <a href="mailto:benjamin.hammond@gmail.com">email me</a>
+            <li>
+              <a href="mailto:benjamin.hammond@gmail.com">email</a>
+            </li>
+          </ul>
         </div>
       </main>
     </div>
