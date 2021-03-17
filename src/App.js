610a68491417729afe5aebb5a8d7544013277490
BenHammondMusic <41567007+benhammondmusic@users.noreply.github.com>
2021-03-16 18:27:59 -0600

    installed tailwind

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <main>
-        <a href="https://docs.google.com/document/d/1nYJf1ZjGetUo8lFbwoyf23prkTw-0H_HiqvMKlY5_nw/edit?usp=sharing">resume - google doc</a>
-        <a href="https://blog.benhammond.tech">blog - music and tech</a>
+      <main className="flex items-center justify-center h-screen">
+        <div className="flex flex-column flex-wrap align-center">
+          <a href="https://docs.google.com/document/d/1nYJf1ZjGetUo8lFbwoyf23prkTw-0H_HiqvMKlY5_nw/edit?usp=sharing">resume - google doc</a>
+          <a href="https://blog.benhammond.tech">blog - music and tech</a>
 
-        <a href="https://github.com/benhammondmusic">github - projects and code playgrounds</a>
+          <a href="https://github.com/benhammondmusic">github - projects and code playgrounds</a>
 
-        <a href="https://www.benhammondmusic.com">benhammondmusic - professional musician site</a>
+          <a href="https://www.benhammondmusic.com">benhammondmusic - professional musician site</a>
 
-        <a href="mailto:benjamin.hammond@gmail.com">email me</a>
+          <a href="mailto:benjamin.hammond@gmail.com">email me</a>
+        </div>
       </main>
     </div>
   );
